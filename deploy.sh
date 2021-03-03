338950b264c864e02d4c50b4e53695dc1e583f78
shimadongxue <793190036@qq.com>
2021-03-03 10:54:12 +0800

    '1、travisy文件修改；2、deploy脚本修改'

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,22 @@
 
 # 确保脚本抛出遇到的错误
 set -e
+
 # 生成静态文件
-npm run docs:build
+npm run build
+
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
+
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f "https://${access_token}@github.com/PanJiaChen/awesome-bookmarks.git" master:gh-pages
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shimadongxue/awesome-bookmarks.git master:gh-pages
+git push -f "https://${access_token}github.com/shimadongxue/book.git" master:gh-pages
 
 cd -
